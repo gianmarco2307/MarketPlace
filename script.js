9dbcab1e6f207ad3549ea3242a8db6b56fb45765
gian <gianmarco.clu23@gmail.com>
2023-12-12 21:06:18 +0100

    implemented login

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const username = '';
+let username = '';
 
 //In questo modo (↓) l'array viene inizializzato con l'array courses presente nel localStorage, altrimenti ne crea uno vuoto
 
@@ -63,3 +63,48 @@ function getCoursesByCategory(category){}
 
 //
 function getCategories(){}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Mi sistemo il login
+    let loginBtn = document.querySelector('#loginBtn')
+
+    let logoutBtn = document.createElement('button');
+    logoutBtn.classList.add('btn');
+    logoutBtn.classList.add('btn-outline-success');
+    logoutBtn.type = 'submit';
+    logoutBtn.id = 'logoutBtn';
+    logoutBtn.textContent = 'Logout'
+    logoutBtn.style.marginLeft = '0.5em'
+
+    let addBtn = document.querySelector('#addCourseBtn');
+
+    loginBtn.addEventListener('click', (event) => {
+        event.preventDefault();  // Impedisce l'azione di default del form
+        let usernameInp = document.querySelector('#usernameInput');
+        let enteredUsername = usernameInp.value.trim();  // Rimuovi eventuali spazi vuoti iniziali o finali
+    
+        if (enteredUsername !== '') {
+            // Se l'username non è vuoto, assegna il valore e esegui le azioni di login
+            username = enteredUsername;
+            console.log(username);
+            // Disabilito il pulsante di login
+            loginBtn.disabled = true;
+            loginBtn.parentElement.appendChild(logoutBtn);
+            addBtn.disabled = false;
+        } else {
+            // Altrimenti, mostra un messaggio di errore o esegui altre azioni desiderate
+            alert('Inserisci un username valido');
+        }
+    });
+    
+
+    logoutBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        username = '';
+        loginBtn.parentElement.removeChild(logoutBtn);
+        loginBtn.disabled = false;
+        addBtn.disabled = true;
+    })
+});
+
